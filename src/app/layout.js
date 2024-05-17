dabebc9e9542a93774fa699efca106139d341519
cyccq <oooo12345545@gmail.com>
2024-05-17 16:39:50 +0800

    修改navbar內容

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Flowbite} from "flowbite-react"; 
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Flowbite>
+        {children}
+      </Flowbite>
     </html>
   );
 }
